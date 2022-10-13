75e90967b825226c9bb755e7c4b39087ba4c8be9
been6505 <62368117+been6505@users.noreply.github.com>
2022-10-14 05:42:36 +0700

    Update udp_server.py

--- udp_server.py
+++ udp_server.py
@@ -4,7 +4,7 @@ import socket
 server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server.bind(("localhost",9999))
 
-message,address = server.recvfrom(2048)
+message,address = server.recvfrom(1024)
 print(message.decode('utf-8'))
 server.sendto("Start connection".encode('utf-8'),address)
 
