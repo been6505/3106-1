5f530ae6f9b17f45bfe2ee89756420d7b5444e30
been6505 <62368117+been6505@users.noreply.github.com>
2022-10-14 08:44:31 +0700

    Update udp_client.py

--- udp_client.py
+++ udp_client.py
@@ -1,10 +1,27 @@
 from http import client
-import socket
+host = "localhost"
+port = 9999
+address = (host,port)
 
 client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-client.sendto("Start Server".encode('utf8'),("localhost",9999))
-print(client.recvfrom(1024)[0].decode('utf8'))
-
+while True:
+    data = input("Enter a word : ")
+    
+    if data == "exit":
+         data = data.encode('utf8')
+         client.sendto(data , address)
+        
+         print("Disconnection")
+         break
+    
+    data = data.encode('utf8')
+    client.sendto(data , address)
+    
+    data , address = client.recvfrom(1024)
+    data = data.decode('utf8')
+    print(f"Server: {data}")
+    
+client.close()
 
 
