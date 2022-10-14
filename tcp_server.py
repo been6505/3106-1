55dc028e6c1e9025a88f0f176ee55fdc9357e2a4
been6505 <62368117+been6505@users.noreply.github.com>
2022-10-14 08:00:16 +0700

    Update tcp_server.py

--- tcp_server.py
+++ tcp_server.py
@@ -1,21 +1,25 @@
 import socket
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(('localhost',9999))
+host = "localhost"
+port = 9999
 
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((host,port))
 server.listen()
 
 while True:
     client,address = server.accept()
     
-    print(f"Connect to server : {address}")
+    print(f"Connect to server : {address[0]} : {address[1]}")
     
-    print(client.recv(1024).decode('utf-8'))
-    client.send("Start connection to server".encode('utf-8'))
     
-    print(client.recv(1024).decode('utf-8'))
-    client.send("End connection".encode('utf-8'))
+    data = client.recv(1024)
+    data = data.decode('utf-8')
+    print(data)
+    
+    data = data.upper()
+    client.send(bytes(data,'utf-8'))
     
     client.close()
-    
     
